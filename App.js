6df119693c4c6cb4af36dea913cbaa910da95699
Aaradhya06 <60058609+Aaradhya06@users.noreply.github.com>
2020-10-09 10:25:30 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,26 @@
+import * as React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+import {Header} from 'react-native-elements';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+      <Header
+      backgroundColor={'#9c8210'}
+      centerComponent={{
+        text:'Monkey Chunky',
+        style:{color:'#fff',fontSize:20},
+      }}
+      />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+ container: {
+    flex: 1,
+    backgroundColor: '#b8b8b8',
+  }
+});
